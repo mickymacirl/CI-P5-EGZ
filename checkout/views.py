9c90a63f2df15b143d2d491c132635a846f0bdc5
mickymacirl <mickymacster@gmail.com>
2023-04-15 19:44:08 +0000

    add profile order history and checkout

--- checkout/views.py
+++ checkout/views.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from cart.contexts import cart_contents
 import stripe
 import json
@@ -132,18 +134,31 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Order was processed! \
-       Your order number is {order_number}. A confirmation \
-       email will be sent to {order.email}.')
 
-#    messages.success(request, f'Order was processed! \
-#        Your order number is {order_number}. A confirmation \
-#        email will be sent to {order_email}.')
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
 
-#    message.success(request, f'Congratulations! Your order has been \
-#        successfully processed with order number {order_number}. We have sent \
-#        a confirmation email to {order_email} shortly. \
-#        Thank you for choosing our service."')
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
 
     if 'cart' in request.session:
         del request.session['cart']
