b4ce02be89ec05c6972e4e97c9ec26d694d6ab0d
mickymacirl <mickymacster@gmail.com>
2023-04-15 17:51:15 +0000

    add propfile app

--- checkout/models.py
+++ checkout/models.py
@@ -5,6 +5,7 @@ from django.db.models import Sum
 from django.conf import settings
 from django_countries.fields import CountryField
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
@@ -12,6 +13,9 @@ class Order(models.Model):
     # of 32 characters. It cannot be null and is not editable. It is used to store a unique identifier
     # for each order, generated using the `_generate_order_number` method.
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
+    full_name = models.CharField(max_length=50, null=False, blank=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
