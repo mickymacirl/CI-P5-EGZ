79240cc198b016372b7438c0e7266b59b51d140e
mickymacirl <mickymacster@gmail.com>
2023-04-16 18:47:09 +0000

    fix 404 template

--- egz/views.py
+++ egz/views.py
@@ -4,6 +4,3 @@ from django.shortcuts import render
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
     return render(request, "errors/404.html", status=404)
-
-
-handler404 = 'egz.views.handler404'
