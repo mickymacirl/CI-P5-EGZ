fb030f3bc74f20008963b1faa9f75309507f3b6e
mickymacirl <mickymacster@gmail.com>
2023-04-16 17:14:24 +0000

    fix default email setting.py

--- egz/settings.py
+++ egz/settings.py
@@ -208,7 +208,7 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'DEFAULT_FROM_EMAIL = 'ethicalgearz@ethicalgearz.com'
+    DEFAULT_FROM_EMAIL = 'ethicalgearz@ethicalgearz.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
