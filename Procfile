7c7c93975be1aaaf2b1fc805bc83a4ed17aae3b5
mickymacirl <mickymacster@gmail.com>
2023-04-16 13:11:03 +0000

    add deployment settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn egz.wsgi:application
--- egz/settings.py
+++ egz/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 """
 Django settings for egz project.
 
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-w&r2i-5(0m=lgtxqh2wd_xw$w6h3uisy$hap=aa044p9si5z*6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['ci-p5-egz.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +121,19 @@ WSGI_APPLICATION = 'egz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
