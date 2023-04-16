b311dc507c6fffdd20508ae2a91cf95ddcad340f
mickymacirl <mickymacster@gmail.com>
2023-04-16 20:04:56 +0000

    create contactus app

---
+++ contactus/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+
+def contactus(request):
+    """ A view to return the contact us page """
+
+    return render(request, 'contactus/index.html')
--- egz/urls.py
+++ egz/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('contactus/', include('contactus.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'egz.views.handler404'
