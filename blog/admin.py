6cf223a88de8c69ee0579f18465193557b280406
mickymacirl <mickymacster@gmail.com>
2023-04-17 15:20:45 +0000

    add blog models and views

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,83 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+# The PostAdmin class inherits from ModelAdmin,
+# and defines a list_display, list_filter,
+# search_fields, and prepopulated_fields
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'created_on',
+                    'category', 'is_pinned', 'status', 'content',)
+    list_filter = ("status", "author", "created_on", 'category', 'is_pinned',)
+    search_fields = ['title', 'content', 'category', 'is_pinned',]
+    prepopulated_fields = {'slug': ('title',)}
+
+    # Add an action to mark posts as published
+    def make_published(self, request, queryset):
+        rows_updated = queryset.update(status='1')
+        if rows_updated == 1:
+            message_bit = "1 post was"
+        else:
+            message_bit = f"{rows_updated} posts were"
+        self.message_user(request, f"{message_bit} successfully published!")
+    make_published.short_description = "Set selected posts to published"
+
+    # Add an action to mark posts as drafts
+    def make_draft(self, request, queryset):
+        rows_updated = queryset.update(status='0')
+        if rows_updated == 1:
+            message_bit = "1 post was"
+        else:
+            message_bit = f"{rows_updated} posts were"
+        self.message_user(
+            request, f"{message_bit} successfully marked as drafts.")
+    make_draft.short_description = "Set selected posts to draft"
+
+    # Add an action to mark posts as disabled
+    def make_disabled(self, request, queryset):
+        rows_updated = queryset.update(status='2')
+        if rows_updated == 1:
+            message_bit = "1 post was"
+        else:
+            message_bit = f"{rows_updated} posts were"
+        self.message_user(
+            request, f"{message_bit} successfully marked as disabled.")
+    make_disabled.short_description = "Set selected posts as disabled"
+
+    # Add a save and continue editing button on the edit form
+    save_on_top = True
+    save_as = True
+    actions = ['make_published', 'make_draft', 'make_disabled']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active', 'status',)
+    list_filter = ('active', 'created_on', 'name', 'post', 'status',)
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+    def mark_as_spam(self, request, queryset):
+        queryset.update(status='spam')
+
+    actions = ['approve_comments', 'mark_as_spam']
+
+    def delete_comments(self, request, queryset):
+        queryset.delete()
+
+    def set_active(self, request, queryset):
+        queryset.update(active=True)
+
+    def set_inactive(self, request, queryset):
+        queryset.update(active=False)
+
+    actions = ['approve_comments', 'mark_as_spam',
+               'delete_comments', 'set_active', 'set_inactive']
+
+
+# Registering the Post model with the PostAdmin class.
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,116 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+"""
+Tuple of tuples
+"""
+STATUS = (
+    (0, "Draft"),
+    (1, "Published"),
+    (2, "Disabled"),
+)
+
+"""
+Post class is a model that has a title, slug, author, updated_on, category,
+content, created_on, # status, and is_pinned
+"""
+msg = 'Only alphabet, spaces and - characters are allowed'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100,
+                             unique=True,
+                             validators=[RegexValidator(
+                                r'^[a-zA-Z\s-]+$', msg)]
+                             )
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts'
+                               )
+    updated_on = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=200, unique=False)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=2)
+    is_pinned = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=200, unique=False)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=2)
+    is_pinned = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-is_pinned', '-id']
+    #    ordering = ['-created_on']
+
+    def save(self, *args, **kwargs):
+        """
+        If the slug field is empty, then add
+        spaces to the title with dashes and save to the slug field.
+        """
+        if not self.slug:
+            self.slug = self.title.replace(' ', '-')
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    """
+    Comment model that has a post, author, name, email, body,
+    created_on, active, and status
+    """
+    PENDING = 0
+    APPROVED = 1
+    REJECTED = 2
+
+    # A tuple of tuples.
+    STATUS_CHOICES = (
+        (PENDING, 'Pending'),
+        (APPROVED, 'Approved'),
+        (REJECTED, 'Rejected'),
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        null=True
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+    """
+    CharField that has a max length of 10, a default value of 'pending', 
+    and a list of choices.
+    """
+    status = models.CharField(max_length=10, default='pending', choices=[
+            ('review', 'Review'), ('accepted', 'Accepted'), ('spam', 'Spam')
+        ])
+
+    class Meta:
+        """
+        Objects will be returned in the order they were created, with the 
+        oldest one first.
+        """
+        ordering = ['created_on']
+
+    def __str__(self):
+        """
+        The __str__ function is a special function that is called when you
+        all str() on an object
+        :return: The body of the comment and the name of the 
+        person who posted it.
+        """
+        return 'Comment {} by {}'.format(self.body, self.name)
